686db70d2baece1bb2cf3737edb1fc22b66b7bcc
alaksandarjesus <alaksandarjesus@yahoo.co.in>
2017-10-02 19:40:48 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+     RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } 
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
